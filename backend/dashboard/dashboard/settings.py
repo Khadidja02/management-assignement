549190eaf3137a3565386af03515ce5bedaed285
Khadidja02 <66958425+Khadidja02@users.noreply.github.com>
2024-12-30 21:25:31 +0100

    Update settings.py

--- backend/dashboard/dashboard/settings.py
+++ backend/dashboard/dashboard/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'django-insecure-i6es*%n)ulq@na9newc@f$h_ms@e5$m%$u90lwy&th7ba2h^j-
 
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOST')
 
 AUTHENTICATION_BACKENDS = [
     "social_core.backends.google.GoogleOAuth2",
